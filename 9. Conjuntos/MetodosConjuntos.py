9fa9e116c8b852b8c415e9e8e7e0735cdb54f0cb
nicolas-sosa <nicolas.sosa@invgate.com>
2022-04-20 09:44:31 -0300

    update de curso

---
+++ 9. Conjuntos/MetodosConjuntos.py
@@ -0,0 +1,13 @@
+conjunto = {1 , 2 , 3 , 4 , 5}
+lista = [1 , 1 , 2 , 3 , 4 , 4 , 5]
+
+# la lista muestra los repetidos el conjunto NO
+
+conjunto.add(20) # agrega el valor
+print(conjunto)
+conjunto.discard(4) # borra el valor = conjunto.remove
+print(conjunto)
+conjunto.pop() # quita valor al azar
+print(conjunto)
+conjunto.clear() # limpia conjunto
+print(conjunto)
---
+++ 9. Conjuntos/conjuntos.py
@@ -0,0 +1,6 @@
+conjunto = {1  , 2  , 3 , 4  , 5 }
+#de este modo queda como un conjunto o tambien
+#conjunto = set[(1, 3, 5, 7)]
+
+print(type(conjunto))
+
